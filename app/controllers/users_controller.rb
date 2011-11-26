6594e5eab4ec665b819402ccccda3ed08b027cc4
Jonathan Abramson <zenpoy@gmail.com>
2011-11-26 23:46:51 +0200

    Conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
   def new
     @user = User.new
     @title = "Sign up"
@@ -22,4 +37,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      #success
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      #failure
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+  
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
 end
