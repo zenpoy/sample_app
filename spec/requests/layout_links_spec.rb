2006746903ee1c81df6794648155eafa39172a55
Jonathan Abramson <zenpoy@gmail.com>
2011-11-26 23:34:40 +0200

    Done with user edit/updat,index and destroy actions

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -50,11 +50,8 @@ describe "LayoutLinks" do
   
   describe "when signed in" do
     before(:each) do
-      @user = Factory(:user)
-      visit signin_path
-      fill_in :email,    :with => @user.email
-      fill_in :password, :with => @user.password
-      click_button
+     @user = Factory(:user)
+     integration_sign_in(@user)
     end
 
     it "should have a signout link" do
@@ -67,6 +64,12 @@ describe "LayoutLinks" do
       visit root_path
       response.should have_selector("a", :href => user_path(@user),
                                          :content => "#{@user.name}'s Profile")
+    end
+    
+    it "should have a settings link" do
+      visit root_path
+      response.should have_selector("a", :href => edit_user_path(@user),
+                                         :content => "Settings")
     end
   end
 end
